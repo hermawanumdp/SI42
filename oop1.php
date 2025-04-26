b36bf3e882697020dc5278011dee62d11f096b74
Nur Rachmat <>
2025-04-26 16:33:57 +0700

    Initial commit

---
+++ oop1.php
@@ -0,0 +1,22 @@
+<?php
+
+class Nilai{
+    private $status;
+
+    public function setStatus($nilai){
+        if($nilai>=50){
+            $this->status="Lulus";
+        }else{
+            $this->status="Tidak Lulus";
+        }
+    }
+
+    public function getStatus(){
+        return $this->status;
+    }
+}
+
+$objekNilai = new Nilai();
+$objekNilai->setStatus(20);
+echo $objekNilai->getStatus();
+?>
